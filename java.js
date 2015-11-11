225ce241716767be97b02ac55bbf45387ff68b53
mohsinrahman <mohsinrahmang@gmail.com>
2015-11-11 13:52:10 +0100

    JS Image slider

---
+++ java.js
@@ -0,0 +1,25 @@
+﻿
+/*function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null  || x == "") {
+        document.getElementById("return").innerHTML="Please enter a username";
+        return false;
+    }
+} */
+
+var myImage = document.getElementById("img");
+
+var imageArray = ["images/bg2.jpg", "images/bg3.jpg", "images/bg5.jpg"];
+
+var imageIndex = 0;
+
+function imageSlider() {
+
+    myImage.setAttribute("src", imageArray[imageIndex]);
+    imageIndex++;
+    if (imageIndex >= imageArray.length) {
+        imageIndex = 0;
+    }
+
+}
+setInterval(imageSlider, 3000);
